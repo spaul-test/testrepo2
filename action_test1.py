cc26ed7ce7a409e02c6b71e1d41d4c96710ceab6
spaul-test <52504808+spaul-test@users.noreply.github.com>
2020-04-12 23:56:19 +0530

    Update action_test1.py

--- action_test1.py
+++ action_test1.py
@@ -1,5 +1,12 @@
 import os
+import json
+
 os.mkdir('pages')
+inp_file = open('data1.json','r')
+data = json.loads(inp_file.read())
+inp_file.close()
+
 f = open('pages/index.md','w')
-f.write("##Hello World!!!")
+#f.write("##Hello World!!!")
+f.write("##Hello {}. Your Age is {}.".format(data["Name"],data["Age"]))
 f.close()
