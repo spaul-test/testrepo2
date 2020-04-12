102f9162dbc8b31d57e516d57976b6da28f6df50
spaul-test <52504808+spaul-test@users.noreply.github.com>
2020-04-12 23:58:32 +0530

    Update action_test1.py

--- action_test1.py
+++ action_test1.py
@@ -3,7 +3,7 @@ import json
 
 os.mkdir('pages')
 inp_file = open('data1.json','r')
-data = json.loads(inp_file.read())
+data = json.loads(str(inp_file.read()))
 inp_file.close()
 
 f = open('pages/index.md','w')
